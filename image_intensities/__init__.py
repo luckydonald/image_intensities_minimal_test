8b77cfd9a1c60ed8f628785b07347489e1cafef0
luckydonald <mac2019.git@luckydonald.de>
2021-08-05 17:42:39 +0200

    Fallback for non-C.

--- image_intensities/__init__.py
+++ image_intensities/__init__.py
@@ -2,4 +2,9 @@ import image_intensities.classes as classes
 import image_intensities.intensities as intensities
 
 from .classes import Luma
-from .intensities import jpeg_intensities, png_intensities
+
+try:
+    from .intensities import jpeg_intensities, png_intensities
+except ImportError:
+    from .pure_python import jpeg_intensities, png_intensities
+# end try
