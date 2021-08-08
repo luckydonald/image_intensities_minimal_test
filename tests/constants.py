50391e496614c97bed3bc23c002312bf92e4fd65
luckydonald <mac2019.git@luckydonald.de>
2021-08-09 01:09:34 +0200

    added example gif file

--- tests/constants.py
+++ tests/constants.py
@@ -17,3 +17,11 @@ EXPECTED_SUMS_PNG = {  # https://derpibooru.org/api/v1/json/images/954482
   "se": 20.831389937866714,
   "sw": 20.76546499989676
 }
+
+IMAGE_PATH_GIF = str(IMAGE_FOLDER / '791101.gif')
+EXPECTED_SUMS_GIF = {  # https://derpibooru.org/api/v1/json/images/791101
+  "ne": 60.06419984738461,
+  "nw": 60.03048984451281,
+  "se": 67.11382688943588,
+  "sw": 64.52939786379487
+}
