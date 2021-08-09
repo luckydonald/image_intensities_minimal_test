8476656bfda51b8eb1a10e1b6245a073d07cdfb2
luckydonald <mac2019.git@luckydonald.de>
2021-08-09 02:07:10 +0200

    Added a test for raw pixel data as well.

---
+++ tests/test_pillow.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+
+import pytest
+import image_intensities.intensities as cffi_compiled
+import image_intensities.pure_python as pure_python
+from constants import EXPECTED_SUMS_PNG, EXPECTED_SUMS_JPG
+from constants import IMAGE_PATH_PNG, IMAGE_PATH_JPG
+from PIL import Image
+
+
+@pytest.mark.parametrize("pixel_bytes_intensities", [cffi_compiled.pixel_bytes_intensities, pure_python.pixel_bytes_intensities])
+def test_pixel_bytes_intensities_png(pixel_bytes_intensities):
+    img = Image.open(IMAGE_PATH_PNG).convert('RGB')
+    result = pixel_bytes_intensities(pixels=img.tobytes(), width=img.width, height=img.height)
+    assert result == EXPECTED_SUMS_PNG
+# end def
+
+
+@pytest.mark.parametrize("pixel_bytes_intensities", [cffi_compiled.pixel_bytes_intensities, pure_python.pixel_bytes_intensities])
+def test_pixel_bytes_intensities_jpeg(pixel_bytes_intensities):
+    img = Image.open(IMAGE_PATH_JPG).convert('RGB')
+    result = pixel_bytes_intensities(
+        pixels=img.tobytes(),
+        width=img.width,
+        height=img.height,
+    )
+    assert result == EXPECTED_SUMS_JPG
+# end def
+
+
+@pytest.mark.parametrize("pixel_bytes_intensities", [cffi_compiled.pixel_bytes_intensities, pure_python.pixel_bytes_intensities])
+def test_pixel_bytes_intensities_png(pixel_bytes_intensities):
+    img = Image.open(IMAGE_PATH_PNG).convert('RGB')
+    result = pixel_bytes_intensities(
+        pixels=img.tobytes(),
+        width=img.width,
+        height=img.height,
+    )
+    assert result == EXPECTED_SUMS_PNG
+# end def
+
+
+@pytest.mark.parametrize("pixel_array_intensities", [cffi_compiled.pixel_array_intensities, pure_python.pixel_array_intensities])
+def test_pixel_array_intensities__tuple_array__png(pixel_array_intensities):
+    img = Image.open(IMAGE_PATH_PNG).convert('RGB')
+    result = pixel_array_intensities(
+        pixels=[img.getpixel(x, y) for y in range(img.height) for x in range(img.width)],
+        width=img.width,
+        height=img.height,
+    )
+    assert result == EXPECTED_SUMS_PNG
+# end def
+
+
+@pytest.mark.parametrize("pixel_array_intensities", [cffi_compiled.pixel_array_intensities, pure_python.pixel_array_intensities])
+def test_pixel_array_intensities__tuple_array__jpg(pixel_array_intensities):
+    img = Image.open(IMAGE_PATH_JPG).convert('RGB')
+    result = pixel_array_intensities(
+        pixels=[img.getpixel(x, y) for y in range(img.height) for x in range(img.width)],
+        width=img.width,
+        height=img.height,
+    )
+    assert result == EXPECTED_SUMS_JPG
+# end def
+
+
+@pytest.mark.parametrize("pixel_array_intensities", [cffi_compiled.pixel_array_intensities, pure_python.pixel_array_intensities])
+def test_pixel_array_intensities__flat_array__png(pixel_array_intensities):
+    img = Image.open(IMAGE_PATH_PNG).convert('RGB')
+    pixels = []
+    for x in range(img.width):
+        for y in range(img.height):
+            r, g, b = img.getpixel(x, y)
+            pixels.append(r)
+            pixels.append(g)
+            pixels.append(b)
+        # end for
+    # end for
+
+    result = pixel_array_intensities(
+        pixels=pixels,
+        width=img.width,
+        height=img.height,
+    )
+    assert result == EXPECTED_SUMS_PNG
+# end def
+
+
+@pytest.mark.parametrize("pixel_array_intensities", [cffi_compiled.pixel_array_intensities, pure_python.pixel_array_intensities])
+def test_pixel_array_intensities__flat_array__png(pixel_array_intensities):
+    img = Image.open(IMAGE_PATH_PNG).convert('RGB')
+    pixels = []
+    for x in range(img.width):
+        for y in range(img.height):
+            r, g, b = img.getpixel(x, y)
+            pixels.append(r)
+            pixels.append(g)
+            pixels.append(b)
+        # end for
+    # end for
+
+    result = pixel_array_intensities(
+        pixels=pixels,
+        width=img.width,
+        height=img.height,
+    )
+    assert result == EXPECTED_SUMS_PNG
+# end def
